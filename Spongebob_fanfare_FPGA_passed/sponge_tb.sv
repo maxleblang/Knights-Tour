6aeec39742ac1bdaaa81288495cbffa031f647e7
Max Leblang <max@leblang.com>
2024-11-18 13:50:04 -0600

    Sponge HW

--- Spongebob_fanfare_FPGA_passed/sponge_tb.sv
+++ Spongebob_fanfare_FPGA_passed/sponge_tb.sv
@@ -3,10 +3,10 @@ module sponge_tb();
 logic clk, rst_n;
 logic go;
 logic piezo, piezo_n;
+int note_cnt;
 
 localparam song_length = 8388608 + 8388608 + 8388608 + 8388608 + 12582912 + 12582912 + 4194304 + 4194304; // Duration of 62914560 clks but with FAST SIM (divide 16)
 localparam fast_song_length = song_length / 16;
-
 
 // Instantiate DUT
 sponge fanfare(.clk(clk), .rst_n(rst_n), .go(go), .piezo(piezo), .piezo_n(piezo_n));
@@ -26,14 +26,26 @@ initial begin
 	go = 0;
 	@(posedge clk);
 	
-	// TODO: fork join
-	while(fanfare.start_note !== 1)
-		@(posedge clk);
+	// Make sure we play every note
+	note_cnt = 0;
+	fork
+		begin: timeout
+			repeat(fast_song_length + 100) @(posedge clk);
+			$display("ERROR: timed out waiting to play the whole song");
+			$stop();
+		end
+		begin
+			@(posedge (note_cnt == 8));
+			disable timeout;
+		end
+	join
 
 	$display("Done running! Go and inspect now");
 	$stop();
 	
 end
+always @(posedge fanfare.start_note) // Count when every note was played
+	note_cnt <= note_cnt + 1;
 always
 	#20 clk = ~clk; // 50MHz clock
 
